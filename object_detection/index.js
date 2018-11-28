613e0d5dc11a17a85260a32dcec0cf8c20409fa2
joaak <29533036+joaak@users.noreply.github.com>
2018-11-28 16:11:01 -0500

    Update index.js

--- object_detection/index.js
+++ object_detection/index.js
@@ -1,9 +1,9 @@
 import my_model, { downloadModel } from './model.js';
 
 const model = downloadModel();
-console.log(model)
+console.log(model);
 const boxes = my_model(model);
-console.log(boxes)
+console.log(boxes);
 
 boxes.forEach(box => {
   const {
