4c35b56930ca4750edd18eec038315d086bc888a
joaak <29533036+joaak@users.noreply.github.com>
2018-11-28 15:44:32 -0500

    Update index.js

--- object_detection/index.js
+++ object_detection/index.js
@@ -21,7 +21,7 @@ const DEFAULT_INPUT = document.getElementById("myCanvas")
  * Downloads a tf.Model
  * @param {String} url Trained Model URL
  */
-export async function downloadModel(model_url = DEFAULT_GRAPH_LOCATION, weights_url = DEFAULT_WEIGHTS_LOCATION) {
+async function downloadModel(model_url = DEFAULT_GRAPH_LOCATION, weights_url = DEFAULT_WEIGHTS_LOCATION) {
   return await tf.loadFrozenModel(model_url, weights_url);
 }
 
@@ -149,5 +149,13 @@ async function my_model(
   console.log(results);
 }
 
-export default my_model;
+const model = await downloadModel();
+const boxes = await my_model(model);
 
+boxes.forEach(box => {
+  const {
+    top, left, bottom, right, classProb, className,
+  } = box;
+
+  drawRect(left, top, right-left, bottom-top, `${className} ${classProb}`)
+});
